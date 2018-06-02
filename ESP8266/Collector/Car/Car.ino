fc4773bc43d1199768da8c3cacad65ad5f3c79fc
老李拉面馆 <SylvesterLi@users.noreply.github.com>
2018-06-02 14:14:41 +0800

    增加了小车控制程序

---
+++ ESP8266/Collector/Car/Car.ino
@@ -0,0 +1,22 @@
+#include <ESP8266WiFi.h>
+
+#define PIN_AO A0  
+#define PIN_DO 4  
+
+
+#define Car_VCC 4 
+#define Car_GND 5 
+void setup() {    
+  pinMode(Car_VCC, OUTPUT);  
+  pinMode(Car_GND, OUTPUT);    
+  Serial.begin(9600);    
+}    
+  
+void loop() {  
+  Serial.print("Car_Start");    
+
+  digitalWrite(Car_VCC,HIGH);
+  digitalWrite(Car_GND,LOW);
+    
+  delay(500);    
+}   
