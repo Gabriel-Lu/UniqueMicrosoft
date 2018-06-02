0cd3d3422203d29ebbc6f169e9bd78a5cef9788b
lihongcang <lihongzhang.power@gmail.com>
2018-06-02 16:36:56 +0800

    修改红外

--- ESP8266/ControlCenter/Controller/Controller.ino
+++ ESP8266/ControlCenter/Controller/Controller.ino
@@ -7,11 +7,12 @@
 #define SCL 5
 #define OLED_RESET 0
 Adafruit_SSD1306 display(OLED_RESET);
+#define D0 16
 
 void setup() 
 {
    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-
+   pinMode(D0,INPUT);
 }
 
 void loop() 
@@ -20,8 +21,17 @@ void loop()
   //display.display();
   //delay(2000);
   //display.clearDisplay();
+  int sensor_red = digitalRead(D0);
+  if(sensor_red)
+  {
+    display.println("person detected");
+    }
+   else
+   {
+      display.println("no person detected");
+   }
   display.setCursor(0,0);
-  display.println("Hello, Arduino!");
+  display.println(sensor_red);
   display.setTextColor(WHITE,BLACK); // 'inverted' text
   display.setTextSize(2);
   display.display();
